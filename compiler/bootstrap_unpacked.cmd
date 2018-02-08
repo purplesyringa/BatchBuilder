8f83188c7b2eab3019c1a45c94e52c18504b245b
Ivanq <imachug@yandex.ru>
2018-02-08 21:40:13 +0300

    Copy contents to TEMP before running if packed=no

--- compiler/bootstrap_unpacked.cmd
+++ compiler/bootstrap_unpacked.cmd
@@ -1,1 +1,6 @@
-"%~dp0contents\%settings_entry%"
+set tmp=%TEMP%\%RANDOM%%RANDOM%%RANDOM%
+mkdir %tmp% >nul 2>nul
+
+robocopy "%~dp0contents" "%tmp%\contents" /E >nul 2>nul
+
+"%tmp%\contents\%settings_entry%"
