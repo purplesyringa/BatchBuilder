59beb5f46427b163d91fd17eadf6d8aca3296715
Ivanq <imachug@yandex.ru>
2018-02-08 16:14:12 +0300

    Add simple compiler

--- build.cmd
+++ build.cmd
@@ -8,12 +8,8 @@ setlocal ENABLEDELAYEDEXPANSION
 	echo 1 >dist\1
 	echo 2 >dist\2
 
-:: Create CAB ::
-	echo .OPTION EXPLICIT >tmp.ddf
-	echo .Set CabinetNameTemplate=data.cab >>tmp.ddf
-	echo .Set Cabinet=on >>tmp.ddf
-	echo .Set Compress=on >>tmp.ddf
-	echo .Set DiskDirectoryTemplate=dist >>tmp.ddf
+:: Compile ::
+	mkdir compiled
 
 	set root=%~dp0src\
 
@@ -21,14 +17,32 @@ setlocal ENABLEDELAYEDEXPANSION
 		set relative=%%a
 		set relative=!relative:%root%=!
 
+		call "%~dp0compile.cmd" "%%a" >"compiled\!relative!"
+	)
+
+:: Create CAB ::
+	echo .OPTION EXPLICIT >tmp.ddf
+	echo .Set CabinetNameTemplate=data.cab >>tmp.ddf
+	echo .Set Cabinet=on >>tmp.ddf
+	echo .Set Compress=on >>tmp.ddf
+	echo .Set DiskDirectoryTemplate=dist >>tmp.ddf
+
+	set root=%~dp0compiled\
+	for /R compiled %%a IN (*) DO (
+		set relative=%%a
+		set relative=!relative:%root%=!
+
 		echo "%%a" >>tmp.ddf
 	)
+
 
 	:: Always add 2 more files because EXTRACT works with >1 files only ::
 	echo "%~dp0dist\1" >>tmp.ddf
 	echo "%~dp0dist\2" >>tmp.ddf
 
 	makecab /F tmp.ddf
+
+	rmdir /S /Q compiled
 
 	del dist\1
 	del dist\2
