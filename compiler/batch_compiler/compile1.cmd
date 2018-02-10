be822632c924ac4eb64a0e100f6ff680ae6125de
Ivanq <imachug@yandex.ru>
2018-02-10 11:34:59 +0300

    Add @directive support

--- compiler/batch_compiler/compile1.cmd
+++ compiler/batch_compiler/compile1.cmd
@@ -1,10 +1,18 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 
+set __directive_safe_recursion__=export
+
 set exporting=BOGUS
 
 for /F "tokens=1,* eol=" %%a IN ('type %1') do (
 	if "%%a" == "export" (
+		call :check_directives "%%a %%b" raw export
+		if not "!ERRORLEVEL!" == "0" (
+			exit /b !ERRORLEVEL!
+		)
+		call :unset_directives export
+
 		call :export_handler %1 %2 "%%a" "%%b"
 	) else (
 		if "%%a" == "return" (
@@ -12,6 +20,11 @@ for /F "tokens=1,* eol=" %%a IN ('type %1') do (
 			rem ->
 			rem if defined __return__ echo "%__return__%=A" >>%__local_storage__%
 			rem exit /b
+
+			call :check_directives "%%a %%b" raw
+			if not "!ERRORLEVEL!" == "0" (
+				exit /b !ERRORLEVEL!
+			)
 
 			:: Make sure something is exported at the moment ::
 				if "!exporting!" == "BOGUS" (
@@ -39,6 +52,11 @@ for /F "tokens=1,* eol=" %%a IN ('type %1') do (
 				rem echo A=B>>%__global_storage__%
 				rem echo A=B>>%__local_storage__%
 
+				call :check_directives "%%a %%b" raw
+				if not "!ERRORLEVEL!" == "0" (
+					exit /b !ERRORLEVEL!
+				)
+
 				echo set %%b
 				echo echo %%b^>^>%%__global_storage__%%
 
@@ -46,18 +64,45 @@ for /F "tokens=1,* eol=" %%a IN ('type %1') do (
 					echo echo %%b^>^>%%__local_storage__%%
 				)
 			) else (
-				if "%%a" == "end" (
-					if "%%b" == "export" (
+				if "%%a" == "@directive" (
+					rem @directive DIR1 DIR2
+					for %%d in (%%b) do (
+						set directive=%%d
+						set value=yes
+						if "!directive:~0,1!" == "-" (
+							set directive=!directive:~1!
+							set value=
+						)
+
+						if defined __directive_!directive!__ (
+							set __active_directive_!directive!__=!value!
+						) else (
+							echo Directive @!directive! does not exist >&2
+							exit /b 1
+						)
+					)
+				) else (
+					set is_end_export=no
+					if "%%a" == "end" if "%%b" == "export" (
+						set is_end_export=yes
+					)
+					if "!is_end_export!" == "yes" (
+						call :check_directives "%%a %%b" raw
+						if not "!ERRORLEVEL!" == "0" (
+							exit /b !ERRORLEVEL!
+						)
+
 						call :end_export_handler %1 %2 "%%a" "%%b"
 					) else (
+						call :check_directives "%%a %%b" raw
+						if not "!ERRORLEVEL!" == "0" (
+							exit /b !ERRORLEVEL!
+						)
+
 						setlocal DISABLEDELAYEDEXPANSION
 						echo %%a %%b
 						endlocal
 					)
-				) else (
-					setlocal DISABLEDELAYEDEXPANSION
-					echo %%a %%b
-					endlocal
 				)
 			)
 		)
@@ -120,5 +165,44 @@ exit /b
 		)
 
 		set exporting=BOGUS
+
+	exit /b
+
+:check_directives
+	for /F "tokens=1,* delims==" %%f in ('set __active_directive 2^>nul') do (
+		set name=%%f
+		set name=!name:__active_directive_=!
+		set name=!name:~0,-2!
+
+		for %%n in (!name!) do (
+			if not "!__directive_%%n__!" == "%~2" (
+				if not "!__directive_%%n__!" == "%~3" (
+					if not "!__directive_%%n__!" == "%~4" (
+						if not "!__directive_%%n__!" == "%~5" (
+							if not "!__directive_%%n__!" == "%~6" (
+								echo Hanging directive @%%n: was used before "%~1", though was expected before "!__directive_%%n__!" >&2
+								exit /b 1
+							)
+						)
+					)
+				)
+			)
+		)
+	)
+
+	exit /b
+
+:unset_directives
+	for /F "tokens=1,* delims==" %%f in ('set __active_directive 2^>nul') do (
+		set name=%%f
+		set name=!name:__active_directive_=!
+		set name=!name:~0,-2!
+
+		for %%n in (!name!) do (
+			if "!__directive_%%n__!" == "%~1" (
+				set %%f=
+			)
+		)
+	)
 
 	exit /b
