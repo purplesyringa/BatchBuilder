b03a311529b0768a42c52bbb2732f9ef8786da6f
Ivanq <imachug@yandex.ru>
2018-02-12 07:26:16 +0300

    Call methods

--- compiler/__class__.cmd
+++ compiler/__class__.cmd
@@ -2,6 +2,7 @@ if "%1" == "new" (
 	set __instance_id__=__instance_%RANDOM%%RANDOM%%RANDOM%__
 )
 if "%1" == "new" (
+	set %__instance_id__%=instance
 	set %__instance_id__%.__type__=%~3
 	call set %__instance_id__%.__origin__=%%__class_%~3_origin__%%
 
@@ -14,3 +15,26 @@ if not "%1" == "batchbuilder" (
 	echo __class__ cannot be called directly.
 	exit /b 1
 )
+
+
+
+rem Get instance ID and method name
+set __instance__=%~2
+set __instance__=%__instance__:batchbuilder_export_=%
+
+set __callee__=%~3
+
+if not defined %__instance__% (
+	echo Instance %__instance__% is not a real instance of any class.
+	exit /b 1
+)
+
+rem Detect class
+call set __class__=%%%__instance__%.__type__%%
+call set __origin__=%%%__instance__%.__origin__%%
+
+shift /1
+shift /1
+shift /1
+
+call %~dp0%__origin__% batchbuilder batchbuilder_export_%__class__%$%__callee__% %1 %2 %3 %4 %5 %6 %7 %8 %9
